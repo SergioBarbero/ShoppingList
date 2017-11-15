43f6bf516981bb662d9cff1419922a40fe5aab55
victorcas04 <victorcas95@gmail.com>
2017-11-15 20:30:10 +0100

    Starting class ChosenProduct

--- src/ChosenProduct.java
+++ src/ChosenProduct.java
@@ -1,2 +1,64 @@
-public class ChosenProduct {
+public class ChosenProduct{
+
+    /**
+     * Product
+     */
+    private Product product;
+
+    /**
+     * Cuantity of this product in list
+     */
+    private int cuantity;
+
+    /**
+     * True if this product allready bought
+     */
+
+    private boolean bought;
+
+    /**
+     * If we have bought this product, we know ohw much it costed
+     */
+
+    private double price;
+
+    /**
+     * We can make this product favorite
+     */
+
+    private boolean favorite;
+
+    /**
+     * Product constructor
+     * @param product object Product
+     * @param cuantity number of this products
+     */
+    public ChosenProduct(Product product, int cuantity){
+        this.bought = false;
+        this.favorite = false;
+        this.product = product;
+        this.cuantity = cuantity;
+    }
+
+    /**
+     * Returns name of my product
+     * @return name
+     */
+    private String getName(){
+        return this.product.getName();
+    }
+
+    /**
+     * Returns id of my product
+     * @return id
+     */
+    public int getId(){
+        return this.product.getId();
+    }
+
+
+    public void increaseCuantity(int id, int c){
+
+    }
+
 }
--- src/Product.java
+++ src/Product.java
@@ -7,21 +7,15 @@ public class Product{
      * Product name
      */
     private String name;
-    /**
-     * Product price
-     */
-    private double price;
 
     /**
      * Product constructor
      * @param id Identifier of my product
      * @param name Name of my product
-     * @param price Price of my product
      */
-    public Product(int id, String name, double price){
+    public Product(int id, String name){
         this.id = id;
         this.name =  name;
-        this.price = price;
     }
 
     /**
@@ -41,35 +35,11 @@ public class Product{
     }
 
     /**
-     * Returns price of my product
-     * @return price
-     */
-    public double getPrice(){
-        return this.price;
-    }
-
-    /**
      * Set name of my product
      * @param name name of my product
      */
     public void setName(String name) {
         this.name = name;
-    }
-
-    /**
-     * Set id of a product
-     * @param id id of a product
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
-     * Set price of a product
-     * @param price price of a product
-     */
-    public void setPrice(double price) {
-        this.price = price;
     }
 
 }
