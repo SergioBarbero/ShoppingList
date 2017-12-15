20cc67f02364394604af10dd32e37baceb8555b4
SergioBarberoBascones <SaQ3Rm7d>
2017-12-15 19:32:25 +0100

    -Created new Stage for new product adding

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -10,8 +10,10 @@ import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -35,28 +37,81 @@ public class MainWindow extends Application  {
 
         BorderPane root = new BorderPane();
 
-        root.setLeft(addVBox());
-        root.setCenter(addGridPane());
+        root.setLeft(actions());
+        root.setCenter(content());
 
         Scene scene = new Scene(root, 900, 900);
 
         primaryStage.setTitle("Shopping List");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        buttonNew.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+
+                Stage newProductStage = new Stage();
+                GridPane newProductPane = new GridPane();
+
+                //Blocking our parent Stage until this one is closed
+                newProductStage.initModality(Modality.WINDOW_MODAL);
+                newProductStage.initOwner(primaryStage);
+
+                Scene scene = new Scene(newProductPane, 900, 200);
+
+                newProductStage.setTitle("New Product");
+                newProductStage.setScene(scene);
+                newProductStage.show();
+
+                newProductPane.setHgap(10);
+                newProductPane.setVgap(10);
+                newProductPane.setPadding(new Insets(10, 10, 10, 10));
+
+                addHeader(newProductPane);
+
+                TextField name = new TextField ();
+                TextField quantity = new TextField ();
+                TextField bought = new TextField ();
+                TextField price = new TextField ();
+                TextField fav = new TextField ();
+                Button submit = new Button("Guardar");
+
+                newProductPane.add(name, 2,1);
+                newProductPane.add(quantity, 3,1);
+                newProductPane.add(bought, 4,1);
+                newProductPane.add(price, 5,1);
+                newProductPane.add(fav, 6,1);
+                newProductPane.add(submit, 7,1);
+
+
+            }
+        });
     }
 
-    public GridPane addGridPane() {
+
+    public GridPane content() {
         GridPane grid = new GridPane();
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(10, 10, 10, 10));
 
-        grid.setGridLinesVisible(true);
+        //grid.setGridLinesVisible(true);
 
-        //Header
+        addHeader(grid);
+
         MenuButton menuButton = new MenuButton("Seleccionar");
         menuButton.getItems().addAll(new MenuItem("Todo"), new MenuItem("Nada"), new MenuItem("Favoritos"), new MenuItem("Comprados"));
         grid.add(menuButton, 1, 0);
+
+        displayContent(grid);
+
+        return grid;
+    }
+
+    public void addHeader(GridPane grid){
+        //Header
+
 
         Text name = new Text("Nombre");
         name.setFont(Font.font("Arial", FontWeight.BOLD, 20));
@@ -78,14 +133,10 @@ public class MainWindow extends Application  {
         fav.setFont(Font.font("Arial", FontWeight.BOLD, 20));
         grid.add(fav, 6, 0);
 
-        displayContent(grid);
-
-        return grid;
     }
 
 
-
-    private VBox addVBox() {
+    private VBox actions() {
         VBox vbox = new VBox();
         vbox.setPadding(new Insets(10));
         vbox.setSpacing(8);
@@ -110,12 +161,7 @@ public class MainWindow extends Application  {
             vbox.getChildren().add(acciones[i]);
         }
 
-        buttonNew.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+
 
         return vbox;
 
@@ -128,18 +174,20 @@ public class MainWindow extends Application  {
             Text quantity = new Text(list.get(i).getQuantity() + "");
             Text bought = new Text(list.get(i).getBoughtToString());
             Text price = new Text(list.get(i).getPriceToString());
+            Text fav = new Text(list.get(i).getFavoriteToString());
             CheckBox selected = new CheckBox("");
-            grid.add(selected, 2, row, 2, 1);
+            grid.add(selected, 1, row, 2, 1);
             grid.add(name, 2, row, 2, 1);
             grid.add(quantity, 3, row, 2, 1);
             grid.add(bought, 4, row, 2, 1);
             grid.add(price, 5, row, 2, 1);
+            grid.add(fav, 6, row, 2, 1);
         }
-
-
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Manager managerGUI = new GUI();
+        managerGUI.loadDB();
         launch(args);
     }
 }
